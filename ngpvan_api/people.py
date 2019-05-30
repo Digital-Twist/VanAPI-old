26d9c0edace3855cd25e3ba683b5cfe71d5b4f8b
Jesse Presnell <JessePresnell@gmail.com>
2019-05-30 11:28:09 -0400

    Adding notes function to person class

--- ngpvan_api/people.py
+++ ngpvan_api/people.py
@@ -32,3 +32,18 @@ class NGPVANPeopleAPI(base.NGPVANAPI):
             data=json.dumps(person_data)
         )
         return {'results': [result], 'person': result.json()}
+    
+    def get_person_notes(self, vanid, params={}):
+        """
+        Params
+        -------
+            text: (str)	Required; the content of the description of the Person.
+            isViewRestricted: (str) Required; set to true if the note should be restricted only to certain users within the current context; set to false if the note may be viewed by any user in the current context.
+            category: (object) Optional; if specified, must be an object whose noteCategoryId corresponds to a Note Category which may be applied to People, and which is available at ,
+        """
+        params['$expand'] = expand
+        response = self.client.get(
+            '%speople/%s/notes' % (self.base_url, vanid),
+            params = params
+        )
+        return {'results': [response], 'person_notes': response.json()}
